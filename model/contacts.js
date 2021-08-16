b6341c8603804d54aa7e1671f5a150ad7201e1ee
Luhivskyi <luhivskyi.d@ukr.net>
2021-08-16 21:49:54 +0300

    retake HW-4

---
+++ model/contacts.js
@@ -0,0 +1,52 @@
+/* eslint-disable semi */
+const Contact = require('./schemas/contact');
+
+const listContacts = async userId => {
+  const results = await Contact.find({ owner: userId }).populate({
+    path: 'owner',
+    select: 'name email subscription -_id',
+  });
+  return results;
+};
+
+const getContactById = async (contactId, userId) => {
+  const result = await Contact.findOne({
+    _id: contactId,
+    owner: userId,
+  }).populate({
+    path: 'owner',
+    select: 'name email subscription -_id',
+  });
+
+  return result;
+};
+
+const removeContact = async (contactId, userId) => {
+  const result = await Contact.findByIdAndRemove({
+    _id: contactId,
+    owner: userId,
+  });
+  return result;
+};
+
+const addContact = async body => {
+  const result = await Contact.create(body);
+  return result;
+};
+
+const updateContact = async (contactId, body, userId) => {
+  const result = await Contact.findByIdAndUpdate(
+    { _id: contactId, owner: userId },
+    { ...body },
+    { new: true },
+  );
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ model/schemas/contact.js
@@ -0,0 +1,31 @@
+/* eslint-disable semi */
+const mongoose = require('mongoose');
+const { Schema, model, SchemaTypes } = mongoose;
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'This fill is required, please fill it'],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, 'This fill is required, please fill it'],
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: [true, 'This fill is required, please fill it'],
+    unique: true,
+    minlength: 10,
+    maxlength: 13,
+  },
+  owner: {
+    type: SchemaTypes.ObjectId,
+    ref: 'user',
+  },
+});
+
+const Contact = model('contact', contactSchema);
+
+module.exports = Contact;
